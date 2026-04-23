6f27ade083d967e21d47899ab00fe44db9b4e315
DrCloy <alsry0712@gmail.com>
2026-04-23 11:51:48 +0900

    fix: 설정 오류 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
@@ -18,7 +18,9 @@ const eslintConfig = defineConfig([
     'next-env.d.ts',
   ]),
   {
-    plugins: importPlugin,
+    plugins: {
+      import: importPlugin,
+    },
   },
   {
     rules: {
@@ -48,7 +50,7 @@ const eslintConfig = defineConfig([
       'import/no-duplicates': 'error',
     },
   },
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs['flat/recommended'],
 ]);
 
 export default eslintConfig;
