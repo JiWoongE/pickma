f5d7a0b4cf3fb734e1b1b0746ff5f90550a5e5a7
DrCloy <alsry0712@gmail.com>
2026-04-25 22:10:32 +0900

    refactor: 전화번호, 이메일을 클릭 가능한 링크로 변경

--- src/components/common/Footer/SupportSection.tsx
+++ src/components/common/Footer/SupportSection.tsx
@@ -8,17 +8,23 @@ export function SupportSection() {
 
       <address className="text-xs font-medium text-gray-600 not-italic">
         <div className="space-y-4">
-          <p className="text-primary-600 flex items-center gap-3 text-base font-semibold lg:gap-2">
+          <a
+            href="tel:15880000"
+            className="text-primary-600 flex items-center gap-3 text-base font-semibold lg:gap-2"
+          >
             <PhoneCall size={20} />
             1588-0000
-          </p>
+          </a>
           <p>평일 09:00 - 18:00 (주말/공휴일 휴무)</p>
         </div>
         <div className="mt-2 space-y-6 border-t border-transparent pt-2 lg:mt-4 lg:space-y-4 lg:border-gray-200 lg:pt-4">
-          <p className="flex items-center gap-2">
+          <a
+            href="mailto:support@example.com"
+            className="flex items-center gap-2"
+          >
             <MailIcon size={16} />
             support@example.com
-          </p>
+          </a>
           <p className="flex items-center gap-2">
             <MessageCircleMoreIcon size={16} />
             {/* TODO: 1:1 문의하기 기능 결정 */}
